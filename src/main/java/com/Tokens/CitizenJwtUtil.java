6244572ef957f0b13f08c8a56a1be7216ad19f04
Dipesh Singh Chadgal <di20587723@wipro.com>
2025-04-29 10:21:11 +0530

    fixed the secret key error

--- src/main/java/com/Tokens/CitizenJwtUtil.java
+++ src/main/java/com/Tokens/CitizenJwtUtil.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CitizenJwtUtil {
-    private static final String SECRET_KEY="Life is a canvas of endless possibilities, painted with the colors of dreams and determination";
+    private static final String SECRET_KEY="Lifeisacanvasofendlesspossibilitiespaintedwiththecolorsofdreamsanddetermination";
     private static final long EXPIRATION_TIME=1000*60*60*12;
 
     public String generateToken(String email){
