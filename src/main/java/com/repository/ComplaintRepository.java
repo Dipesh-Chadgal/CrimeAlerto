7d4d6c416e38cc4a08adbad3f41478b198fc8b79
Dipesh Singh Chadgal <di20587723@wipro.com>
2025-05-22 14:31:25 +0530

    updated

--- src/main/java/com/repository/ComplaintRepository.java
+++
@@ -1,8 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.entity.Complaint;
-
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-    // Additional custom methods if needed
-}
